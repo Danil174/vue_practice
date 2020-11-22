97e396de4208b63747f476f6e857bb523b79486f
danil milostiviy <milostiviy.danil@yandex.ru>
2020-11-22 16:23:17 +0300

    add firebase

--- README.md
+++ README.md
@@ -22,3 +22,7 @@ npm run lint
 
 ### Customize configuration
 See [Configuration Reference](https://cli.vuejs.org/config/).
+
+### Test User for firebase:
+test2211@mail.ru 
+test16
--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import Vue from 'vue';
 import Vuelidate from 'vuelidate';
+import firebase from 'firebase/app';
 import App from './App.vue';
 import router from './router';
 import store from './store';
@@ -7,6 +8,8 @@ import dateFilter from './filters/date.filter';
 import messagePlugin from './utils/message.plugin';
 import './registerServiceWorker';
 import 'materialize-css/dist/js/materialize.min';
+import 'firebase/auth';
+import 'firebase/database';
 
 Vue.config.productionTip = false;
 
@@ -14,8 +17,24 @@ Vue.use(messagePlugin);
 Vue.use(Vuelidate);
 Vue.filter('date', dateFilter);
 
-new Vue({
-  router,
-  store,
-  render: (h) => h(App),
-}).$mount('#app');
+firebase.initializeApp({
+  apiKey: 'AIzaSyBANZsamneE2l_m-deGmJOoE0PnvNqUtlA',
+  authDomain: 'vue-practice-6ef34.firebaseapp.com',
+  databaseURL: 'https://vue-practice-6ef34.firebaseio.com',
+  projectId: 'vue-practice-6ef34',
+  storageBucket: 'vue-practice-6ef34.appspot.com',
+  messagingSenderId: '127076171809',
+  appId: '1:127076171809:web:1af26a935f84d551fe6360',
+});
+
+let app;
+
+firebase.auth().onAuthStateChanged(() => {
+  if (!app) {
+    app = new Vue({
+      router,
+      store,
+      render: (h) => h(App),
+    }).$mount('#app');
+  }
+});
